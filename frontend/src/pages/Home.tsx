fde36bec4b3abee9006f5677911bcc41a4e9e637
Sam Alan <145153007+salan7@users.noreply.github.com>
2026-09-09 00:03:11 -0300

    Moved dummy player data over to the Flask backend, set react to generate player cards using that instead of frontend dummy data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import PlayerCard from "../components/PlayerCard";
-import {testPlayers} from "../data/testPlayers";
 import { useEffect, useState} from 'react';
-import { Typography } from '@mui/material';
+import type { Player } from '../types/Player';
 
 interface HomeProps {
   searchQuery: string;
@@ -11,26 +10,28 @@ interface HomeProps {
 
 function Home({ searchQuery }: HomeProps){
 
-  const filteredPlayers = testPlayers.filter(
+  
+
+  const [players, setPlayers] = useState<Player[]>([]);
+
+  useEffect(() => {
+  fetch("http://127.0.0.1:5000/api/players")
+    .then((response) => response.json())
+    .then((data) => setPlayers(data));
+  }, []);
+
+  const filteredPlayers = players.filter(
     (player) => player.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-  fetch("http://127.0.0.1:5000/api/test")
-    .then((response) => response.json())
-    .then((data) => setMessage(data.message));
-}, []);
-
   return (
     <Container>
-      <Typography>
-        {message}
-      </Typography>
       <Grid container spacing = {2}>
         {filteredPlayers.map((player) => (
-          <Grid size = {{ xs: 12, sm: 6, md: 4}}>
+          <Grid 
+          size = {{ xs: 12, sm: 6, md: 4}}
+          key={player.name}
+          >
             <PlayerCard
               player={player}
               onClick={() => console.log("Player clicked")}  
