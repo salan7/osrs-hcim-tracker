f1cc66350c60535a4acf46fe1539554960fffa52
Sam Alan <145153007+salan7@users.noreply.github.com>
2026-09-08 23:22:33 -0300

    Added Flask integration for backend handling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import PlayerCard from "../components/PlayerCard";
 import {testPlayers} from "../data/testPlayers";
+import { useEffect, useState} from 'react';
+import { Typography } from '@mui/material';
 
 interface HomeProps {
   searchQuery: string;
@@ -13,8 +15,19 @@ function Home({ searchQuery }: HomeProps){
     (player) => player.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+  fetch("http://127.0.0.1:5000/api/test")
+    .then((response) => response.json())
+    .then((data) => setMessage(data.message));
+}, []);
+
   return (
     <Container>
+      <Typography>
+        {message}
+      </Typography>
       <Grid container spacing = {2}>
         {filteredPlayers.map((player) => (
           <Grid size = {{ xs: 12, sm: 6, md: 4}}>
