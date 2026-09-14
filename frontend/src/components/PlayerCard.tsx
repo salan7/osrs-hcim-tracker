f133ae468355d03361ae8db9adbbd864386256d6
Sam Alan <145153007+salan7@users.noreply.github.com>
2026-09-13 22:57:58 -0300

    Added styling for CardContent of PlayerCard

--- frontend/src/components/PlayerCard.tsx
+++ frontend/src/components/PlayerCard.tsx
@@ -5,6 +5,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from "@mui/material";
 import statsImage from "../assets/stats.png";
+import cardInfo_Background from "../assets/cardInfo_Background.png"
 
 
 interface PlayerCardProps {
@@ -22,11 +23,49 @@ function PlayerCard({player, onClick}: PlayerCardProps) {
         height="500"
         image={statsImage}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+        <CardContent
+        sx={{
+          backgroundImage: `url(${cardInfo_Background})`,
+          backgroundRepeat: "repeat",
+          border: "4px solid black"
+        }}>
+          <Typography 
+          gutterBottom 
+          variant="h5" 
+          component="div"
+          sx={{
+          color: "yellow",
+          fontSize: "35px",
+          textShadow: `
+            -1px -1px 0 black,
+             0px -1px 0 black,
+             1px -1px 0 black,
+              -1px  0px 0 black,
+             1px  0px 0 black,
+             -1px  1px 0 black,
+              0px  1px 0 black,
+             1px  1px 0 black
+         `,
+        }}>
             {player.name}
           </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary'}}>
+          <Typography 
+          variant="body2" 
+          sx={{
+          color: "yellow",
+          fontSize: "25px",
+          textShadow: `
+            -1px -1px 0 black,
+             0px -1px 0 black,
+             1px -1px 0 black,
+              -1px  0px 0 black,
+             1px  0px 0 black,
+             -1px  1px 0 black,
+              0px  1px 0 black,
+             1px  1px 0 black
+         `,
+        }}
+          >
             Rank: {player.hcim_rank ?? "N/A"}
           </Typography>
 
